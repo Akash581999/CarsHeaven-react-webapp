683156fc07c68f37a3b0a67ab4c6d1d4737285af
Akash Kumar <73817248+Akash581999@users.noreply.github.com>
2024-07-23 18:26:44 +0530

    Toast modified

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 // import { Toaster, toast } from 'sonner';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     {/* <Toaster/> */}
-    <ToastContainer/>
+    {/* <ToastContainer /> */}
   </React.StrictMode>,
 )
